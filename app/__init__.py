90928c131aae219536349a100d1413d238a51765
rafael <lugo31625@gmail.com>
2024-02-20 17:25:38 -0500

    base de datos con python

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+from .config import Config
+##crear objeto de aplicacion  
+app = Flask(__name__)
+
+#configurar el objeto flask
+app.config.from_object(Config)
+
+##crear un objeto sql
+db = SQLAlchemy(app)
+
+##objeto para migraciones
+migrate =   Migrate(app, db)
+
+##importar modelos
+from .models import Doctores
+
+##ejecutar el objeto
+if __name__ == '__main__':
+    app.run()
